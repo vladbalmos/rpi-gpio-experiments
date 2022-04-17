85657f288ece6b9d98e8121baf907486e30338ee
Vlad Balmos <vladbalmos@yahoo.com>
2022-04-17 18:03:50 +0300

    Add temp offset

--- temp-humid-display.py
+++ temp-humid-display.py
@@ -52,6 +52,7 @@ def sensor_reading():
     read_msg = I2C.Message([0x0, 0x0, 0x0, 0x0], read = True)
     bus.transfer(SENSOR_ADDRESS, [read_msg])
     raw_data = read_msg.data
+    print(raw_data)
 
     # first 2 bits represent the response status
     reading_status = raw_data[0] >> 6
@@ -65,7 +66,8 @@ def sensor_reading():
 
     # next 14 bits represent the temperature
     temp = (raw_data[2] << 6) | raw_data[3]
-    temp = temp / (2 ** 14 - 2) * 165 - 40
+    print(temp)
+    temp = (temp / (2 ** 14 - 2) * 165 - 40) - 3
 
     return (humidity, temp)
 
